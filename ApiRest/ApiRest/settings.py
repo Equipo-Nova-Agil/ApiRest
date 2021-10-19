14d2844307a8a582ffe5eece9646862ff930ee56
AngelaAcosta31 <angelamaria731@hotmail.com>
2021-10-18 19:49:31 -0500

    cambiando puerto

--- ApiRest/ApiRest/settings.py
+++ ApiRest/ApiRest/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'ApiRest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST':'mysql.server',
-        'PORT':'',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
         'USER':'root',
         'PASSWORD': 'angela',
         'NAME':'misiontic',
